0ed3fc0fe0fe3f4f4942d4fb7c814fad0c91fa41
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-07-11 23:44:27 -0400

    redux boilerplate layout part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import Post from './components/Posts';
 import PostForm from './components/PostForm';
+import { Provider } from 'react-redux';
+import store from './store';
+
+
 
 class App extends Component {
-  
-  
   render() {
     
     return (
-      <div className="App">
-        <PostForm/>
-        <Post/>
-      </div>
+      <Provider store = {store}>
+        <div className="App">
+          <PostForm/>
+          <Post/>
+        </div>
+      </Provider>
+      
     );
   }
 }
